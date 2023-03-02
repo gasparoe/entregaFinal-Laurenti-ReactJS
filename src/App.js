25f99167e9a76b5e27a21a7410dbe20a920c001c
Gaspar Laurenti <laurenti8@hotmail.com>
2023-03-02 19:16:49 -0300

    Limpieza de codigo

--- src/App.js
+++ src/App.js
@@ -6,35 +6,31 @@ import ItemListContainer from "./Components/ItemListContainer/ItemListContainer"
 import ItemDetail from "./Components/ItemDetail/ItemDetail";
 import SearchProduct from "./Components/SearchProduct/SearchProduct";
 import Carrito from "./Components/Carrito/Carrito";
-import Toastify from 'toastify-js'
-import "toastify-js/src/toastify.css"
+import Toastify from "toastify-js";
+import "toastify-js/src/toastify.css";
 
 const App = () => {
-
-
-  const [carrito, setCarrito] = useState([])
+  const [carrito, setCarrito] = useState([]);
 
   const agregarCarrito = (item) => {
-    setCarrito([...carrito,item])
+    setCarrito([...carrito, item]);
     Toastify({
-
       text: "Producto agregado al carrito",
-      
+
       duration: 3000,
       style: {
         background: "linear-gradient(to right, #EE1A1A, #EE6E1A)",
       },
-      
-      }).showToast();
-  }
+    }).showToast();
+  };
 
   const vaciarCarrito = () => {
-    setCarrito([])
-  }
+    setCarrito([]);
+  };
 
   return (
     <BrowserRouter>
-      <Navbar cantidadCarrito={carrito.length}/>
+      <Navbar cantidadCarrito={carrito.length} />
       <Routes>
         <Route exact path="/" element={<ItemListContainer />}></Route>
         <Route
@@ -42,9 +38,17 @@ const App = () => {
           path="/category/:idCategoria"
           element={<ItemListContainer />}
         ></Route>
-        <Route exact path="/item/:id" element={<ItemDetail agregar={agregarCarrito} />}></Route>
+        <Route
+          exact
+          path="/item/:id"
+          element={<ItemDetail agregar={agregarCarrito} />}
+        ></Route>
         <Route exact path="/search/:name" element={<SearchProduct />}></Route>
-        <Route exact path="/carrito" element={<Carrito carrito = {carrito} vaciar={vaciarCarrito} />}></Route>
+        <Route
+          exact
+          path="/carrito"
+          element={<Carrito carrito={carrito} vaciar={vaciarCarrito} />}
+        ></Route>
       </Routes>
       <Footer />
     </BrowserRouter>
